5dbddd7d0582fca688a8a47d21963c2941967055
karina <kguerrerofernandez@instructors.2u.com>
2024-12-03 19:14:57 -0800

    add relationships between models

---
+++ server/src/models/index.ts
@@ -0,0 +1,32 @@
+import sequelize from "../config/connection.js";
+import { UserFactory } from "./User.js";
+import { AuctionedItemFactory } from "./AuctionedItem.js";
+import { ItemizedFactory } from "./Itemized.js";
+import { PushFactory } from "./Push.js"
+
+// Initialize the models
+const User = UserFactory(sequelize);
+const AuctionedItem = AuctionedItemFactory(sequelize);
+const Itemized = ItemizedFactory(sequelize);
+const Push = PushFactory(sequelize);
+
+User.hasMany(Push, {
+    onDelete: 'ERROR',
+});
+
+Push.belongsTo(User);
+
+AuctionedItem.hasMany(Push, {
+    onDelete: 'ERROR',
+});
+
+Push.belongsTo(AuctionedItem);
+
+AuctionedItem.hasMany(Itemized, {
+    onDelete: 'CASCADE',
+});
+
+Itemized.belongsTo(AuctionedItem);
+
+export { sequelize, AuctionedItem, Itemized, Push, User };
+
